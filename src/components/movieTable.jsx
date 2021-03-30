cbe3c843ce3cc664fdcc09aa6b384cf0adc7ff0a
Jonas <jonaswessner@gmx.de>
2021-03-30 20:19:30 +0200

    fix: make sure that the page number is always valid; In order to achive that we make sure that we bring the object into an valid state after each modification i.e. every time an event-handler is called. Then the render-method can simply display this state

--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -4,29 +4,40 @@ import GenrePicker from "./genrePicker";
 import Like from "./like";
 import Pagination from "./pagination";
 
-// TODO: When we are on a certain page and then change
-// the genre the page numbers do not change. This can result
-// in that we are on an invalid page, if the former page does not exist anymore.
-// We have solved the same problem when deleting a movie with the getValidCurrentPageIndex-Method.
-// We must find a way to centrally solve this issue.
 class MovieTable extends Component {
   state = {
     pageSize: 3,
     currentPageIndex: 0,
     currentGenreId: null,
     movies: undefined,
+    filteredMovies: undefined,
+    paginatedMovies: undefined,
   };
 
   constructor() {
     super();
     this.state.movies = getMovies();
+    this.state.filteredMovies = this.state.movies; // no filters to start with
+    this.state.paginatedMovies = this.paginate(
+      this.state.filteredMovies,
+      this.state.pageSize,
+      this.state.currentPageIndex
+    );
   }
 
-  handleDelete = index => {
-    const movies = this.state.movies.filter((elem, i) => i !== index);
-    const { pageSize, currentPageIndex } = this.state;
+  handleDelete = id => {
+    const movies = this.state.movies.filter(mov => mov._id !== id);
+    const { pageSize, currentPageIndex, currentGenreId } = this.state;
+    const filteredMovies = this.filter(movies, currentGenreId);
+    const paginatedMovies = this.paginate(
+      filteredMovies,
+      pageSize,
+      currentPageIndex
+    );
     this.setState({
       movies: movies,
+      filteredMovies: filteredMovies,
+      paginatedMovies: paginatedMovies,
       currentPageIndex: this.getValidCurrentPageIndex(
         movies.length,
         pageSize,
@@ -44,95 +55,128 @@ class MovieTable extends Component {
   };
 
   handlePageChanged = index => {
-    this.setState({ currentPageIndex: index });
+    const { movies, pageSize } = this.state;
+    this.setState({
+      currentPageIndex: index,
+      paginatedMovies: this.paginate(movies, pageSize, index),
+    });
   };
 
   handleGenreChanged = genre => {
+    const { movies, pageSize, currentPageIndex } = this.state;
+    const filteredMovies = this.filter(movies, genre._id);
+    const validPageIndex = this.getValidCurrentPageIndex(
+      filteredMovies.length,
+      pageSize,
+      currentPageIndex
+    );
+    const paginatedMovies = this.paginate(
+      filteredMovies,
+      pageSize,
+      validPageIndex
+    );
     console.log(genre);
     this.setState({
       currentGenreId: genre._id,
+      filteredMovies: filteredMovies,
+      paginatedMovies: paginatedMovies,
+      currentPageIndex: validPageIndex,
     });
   };
 
   getValidCurrentPageIndex = (itemsCount, pageSize, currentPageIndex) => {
     const pages = Math.ceil(itemsCount / pageSize);
-    return currentPageIndex < pages ? currentPageIndex : pages - 1;
+    if (pages === 0) {
+      return 0;
+    }
+    return currentPageIndex < pages && currentPageIndex >= 0
+      ? currentPageIndex
+      : pages - 1;
   };
 
-  getFilteredMovies = movies => {
-    const { currentGenreId } = this.state;
+  filter = (movies, currentGenreId) => {
     return movies.filter(
       (movie, i) =>
         currentGenreId === null || movie.genre._id === currentGenreId
     );
   };
 
-  getRenderedMovies = movies => {
-    const { pageSize, currentPageIndex } = this.state;
+  paginate = (movies, pageSize, currentPageIndex) => {
     return movies.filter(
       (m, index) => Math.floor(index / pageSize) === currentPageIndex
     );
   };
 
   render() {
-    const { pageSize, currentPageIndex, movies } = this.state;
-    const filteredMovies = this.getFilteredMovies(movies);
+    const {
+      pageSize,
+      currentPageIndex,
+      currentGenreId,
+      movies,
+      filteredMovies,
+      paginatedMovies,
+    } = this.state;
     const size = filteredMovies.length;
-    const renderedMovies = this.getRenderedMovies(filteredMovies);
-
-    if (size === 0) return <p>There are currently no more movies</p>;
 
     return (
       <div className="movie-table">
         <GenrePicker
-          selectedGenreId={this.state.currentGenreId}
+          selectedGenreId={currentGenreId}
           onStateChanged={this.handleGenreChanged}
         />
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
-              <th scope="col"></th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {renderedMovies.map((movie, index) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    onToggle={this.handleLikeToggle}
-                    bindingContext={movie}
-                    isEnabled={movie.isLiked}
-                  />
-                </td>
-                <td>
-                  <button
-                    id={index}
-                    onClick={() => this.handleDelete(index)}
-                    className="btn btn-danger"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination
-          itemCount={size}
-          pageSize={pageSize}
-          currentPageIndex={currentPageIndex}
-          onStateChanged={this.handlePageChanged}
-        />
-        <p>Showing {size} movies in the database</p>
+        {size <= 0 ? (
+          <p>There are currently no more movies</p>
+        ) : (
+          <React.Fragment>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Genre</th>
+                  <th scope="col">Stock</th>
+                  <th scope="col">Rate</th>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {paginatedMovies.map((movie, index) => (
+                  <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                      <Like
+                        onToggle={this.handleLikeToggle}
+                        bindingContext={movie}
+                        isEnabled={movie.isLiked}
+                      />
+                    </td>
+                    <td>
+                      <button
+                        id={movie._id}
+                        onClick={() => this.handleDelete(movie._id)}
+                        className="btn btn-danger"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <Pagination
+              itemCount={size}
+              pageSize={pageSize}
+              currentPageIndex={currentPageIndex}
+              onStateChanged={this.handlePageChanged}
+            />
+            <p>
+              Showing {size}/{movies.length} movies in the database
+            </p>
+          </React.Fragment>
+        )}
       </div>
     );
   }
