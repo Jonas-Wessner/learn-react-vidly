b49318a81fdc8512feae38e15f08e058d632eb6f
Jonas <jonaswessner@gmx.de>
2021-04-06 15:11:47 +0200

    feat: implement validation using joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./input";
+import Joi, { errors } from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -7,22 +8,34 @@ class LoginForm extends Component {
     // of the inputs in order to be able to access them
     // using the bracket-notation in the handleChange-method
     account: { username: "", password: "" },
+    // keys are names of the properties, values are messages about the error: e.g. {username: "Username cannot be empty"}
     errors: {},
   };
 
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
   validate = () => {
-    const { username, password } = this.state.account;
+    const options = { abortEarly: false }; // do not stop after first error
+    const { error } = Joi.validate(this.state.account, this.schema, options);
     const errors = {};
 
-    if (username.trim() === "") {
-      errors.username = "Username is required";
-    }
+    if (!error) return errors; // cannot iterate over null or undefined, which is the case when we have no errors
 
-    if (password.trim() === "") {
-      errors.password = "Password is required";
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message; // break down complex error object into simpler custom error object
     }
-
     return errors;
+  };
+
+  // this method validates only the given property
+  validateProperty = ({ name, value }) => {
+    const toValidate = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(toValidate, schema);
+    return error ? error.details[0].message : null;
   };
 
   /* By default the submission of a form results in a full page reload.
@@ -41,9 +54,17 @@ class LoginForm extends Component {
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) {
+      errors[input.name] = errorMessage; // set error if present
+    } else {
+      delete errors[input.name]; // clear error if not present
+    }
+
     const account = { ...this.state.account };
     account[input.id] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
