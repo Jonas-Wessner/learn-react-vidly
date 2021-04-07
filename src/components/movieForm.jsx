6fe87dfd69e4cc878b5733967358cda75a2d1c7f
Jonas <jonaswessner@gmx.de>
2021-04-07 20:10:46 +0200

    feat: add movieForm, implement minValue, maxValue, integer, number function in the Validator-class

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,20 +1,70 @@
-import React, { Component } from "react";
+import React from "react";
+import Validator from "../modules/validator";
+import Form from "./form";
 
-class MovieForm extends Component {
-  handleSave = () => {
-    this.props.history.push("/movies"); // redirect to movies page after save
+class MovieForm extends Form {
+  constructor() {
+    super();
+    const obj = {
+      data: {
+        title: "",
+        genre: "",
+        numberInStock: "",
+        rate: "",
+      },
+    };
+    // add this, do not override properties of Form
+    Object.assign(this.state, obj);
+  }
+
+  title = "Movie Form";
+  buttonLabel = "Save";
+
+  schema = {
+    title: new Validator().notEmpty().setLabel("Title"),
+    genre: new Validator().notEmpty().setLabel("Genre"),
+    numberInStock: new Validator()
+      .notEmpty()
+      .integer()
+      .minValue(0)
+      .maxValue(100)
+      .setLabel("Number In Stock"),
+    rate: new Validator()
+      .notEmpty()
+      .number()
+      .minValue(0)
+      .maxValue(10)
+      .setLabel("Rate"),
   };
 
-  render() {
+  handleSubmit = () => {
+    // Call Server and redirect user
+    console.log("Submitted");
+  };
+
+  renderContent = () => {
     return (
       <React.Fragment>
-        <h1>Movie Form {this.props.match.params.id}</h1>
-        <button className="btn btn-primary" onClick={this.handleSave}>
-          Save
-        </button>
+        {this.renderInput({
+          name: "title",
+          label: "Title",
+          autoFocus: true,
+        })}
+        {this.renderInput({
+          name: "genre",
+          label: "Genre",
+        })}
+        {this.renderInput({
+          name: "numberInStock",
+          label: "Number In Stock",
+        })}
+        {this.renderInput({
+          name: "rate",
+          label: "Rate",
+        })}
       </React.Fragment>
     );
-  }
+  };
 }
 
 export default MovieForm;
--- src/modules/validator.jsx
+++ src/modules/validator.jsx
@@ -58,13 +58,18 @@ class Validator {
     return true;
   };
 
+  getLabel = alternative => {
+    const label = this.#label ? this.#label : alternative;
+    return `"${label}"`;
+  };
+
   /**
    *  specifies that a property cannot be null, undefined, an empty array, or an empty object
    * @returns the current Object => chainable
    */
   notEmpty = () => {
     this.validatorFunctions.push((value, key) => {
-      const message = `${this.#label || key} is not allowed to be empty`;
+      const message = `${this.getLabel(key)} is not allowed to be empty`;
       if (!value || value === "" || empty(value)) {
         return message;
       }
@@ -75,9 +80,9 @@ class Validator {
 
   minLength = min => {
     this.validatorFunctions.push((value, key) => {
-      const message = `${
-        this.#label || key
-      } must have a minimum length of ${min} characters`;
+      const message = `${this.getLabel(
+        key
+      )} must have a minimum length of ${min} characters`;
 
       console.log(value, min);
       return value.length < min ? message : null;
@@ -87,11 +92,33 @@ class Validator {
 
   maxLength = max => {
     this.validatorFunctions.push((value, key) => {
-      const message = `${
-        this.#label || key
-      } must have a maximum length of ${max} characters`;
+      const message = `${this.getLabel(
+        key
+      )} must have a maximum length of ${max} characters`;
 
       return value.length > max ? message : null;
+    });
+    return this;
+  };
+
+  minValue = min => {
+    this.validatorFunctions.push((value, key) => {
+      const message = `${this.getLabel(
+        key
+      )} must be greater than or equal to ${min}`;
+
+      return value < min ? message : null;
+    });
+    return this;
+  };
+
+  maxValue = max => {
+    this.validatorFunctions.push((value, key) => {
+      const message = `${this.getLabel(
+        key
+      )} must be less than or equal to ${max}`;
+
+      return value > max ? message : null;
     });
     return this;
   };
@@ -99,11 +126,35 @@ class Validator {
   email = () => {
     this.validatorFunctions.push((value, key) => {
       if (!value.includes("@")) {
-        const message = `${this.#label || key} is not a valid email address`;
+        const message = `"${this.getLabel(key)}" is not a valid email address`;
         return message;
       }
       return null;
     });
+    return this;
+  };
+
+  integer = () => {
+    this.validatorFunctions.push((value, key) => {
+      const message = `${this.getLabel(key)} must be an integer`;
+      const int = parseInt(value);
+      const float = parseFloat(value);
+
+      // parseInt also works with floats, if we compare it to the float we can see if the value initially was a float or not
+      return !int || int !== float ? message : null;
+    });
+    return this;
+  };
+
+  number = () => {
+    this.validatorFunctions.push((value, key) => {
+      const message = `${this.getLabel(key)} must be a number`;
+      const float = parseFloat(value);
+
+      // parseInt also works with floats, if we compare it to the float we can see if the value initially was a float or not
+      return !float ? message : null;
+    });
+    console.log(this.validatorFunctions);
     return this;
   };
 
